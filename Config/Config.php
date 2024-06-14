e0f8e27b86985ed559ae60d2b5b1048e3fa7c9dc
AHYAR PRATAMA <147960017+MAhyarPratama@users.noreply.github.com>
2024-06-14 17:35:33 +0800

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=comikku', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
+}
+
+// Include model and service
+include_once (__DIR__ . '/../Models/ComicsModel.php');
+include_once (__DIR__ . '/../Services/ComicsService.php');
+
+// Initialize model and service instances
+$comicsModel = new ComicsModel($db);
+$comicsService = new ComicsService($comicsModel);
+?>
