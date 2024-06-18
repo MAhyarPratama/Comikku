83247b8e13635fe94501446f56edbe48f673c63b
AHYAR PRATAMA <147960017+MAhyarPratama@users.noreply.github.com>
2024-06-18 17:42:55 +0800

    Update AuthServices.php

--- Services/AuthServices.php
+++ Services/AuthServices.php
@@ -11,19 +11,25 @@ class AuthService
     }
 
     public function verifyCredentials($username, $password) {
-        return $this->userModel->findUser($username, $password);
-    }
-
+        $user = $this->userModel->getUserByUsername($username);
+        var_dump($password); // Tambahkan untuk melihat password yang diberikan
+        var_dump($user['Password']); // Tambahkan untuk melihat password dari database
+    
+        if ($user && $password === $user['Password']) {
+            return $user;
+        }
+        return false;
+    } 
+    
     public function authenticate($username, $password)
     {
         $user = $this->userModel->getUserByUsername($username);
-        if ($user && $user['PasswordHash'] === md5($password))
-        {
+        if ($user && $password === $user['password']) {
             return true;
         }
-        return false;   
+        return false;
     }
-
+    
     public function getUserByUsername($username)
     {
         return $this->userModel->getUserByUsername($username);
